bf99e1f5ca62ddc934fdfc55bb3d7eea75506ac8
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-08-31 00:34:57 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["YeYaYv"]
+OWNER_NAME = ".𝗦𝗮𝗟𝗮𝗛 - ويفي"
+BOT_TOKEN = "7122510172:AAFMsGL8Hn98o9fcgxQvkVekI0Lp6aR2nhI"
+DATABASE = "mongodb+srv://devfraon:devfraon@devfraon.sg3sijf.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/Wevy_Python"
+GROUP = "https://t.me/SQ_RP"
+VID_SO = "https://telegra.ph/file/3b8d5a2d60165d160b96c.jpg"
+PHOTO = "https://telegra.ph/file/3b8d5a2d60165d160b96c.jpg"
+LOGS = "ta5zen"
